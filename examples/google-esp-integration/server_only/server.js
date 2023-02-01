e5b27b942db5e1cc76a79e09184bc86df517baf2
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-02-01 16:12:11 +1100

    fix the example

--- examples/google-esp-integration/server_only/server.js
+++ examples/google-esp-integration/server_only/server.js
@@ -21,11 +21,11 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-const axios = require('axios');
-const session = require('cookie-session');
-const ejs = require('ejs');
-const express = require('express');
-const nocache = require('nocache');
+const axios = require("axios");
+const session = require("cookie-session");
+const ejs = require("ejs");
+const express = require("express");
+const nocache = require("nocache");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,21 +33,23 @@ const port = process.env.PORT || 3000;
 const uid2BaseUrl = process.env.UID2_BASE_URL;
 const uid2ApiKey = process.env.UID2_API_KEY;
 
-app.use(session({
-  keys: [ process.env.SESSION_KEY ],
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
+app.use(
+  session({
+    keys: [process.env.SESSION_KEY],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('.html', ejs.__express);
-app.set('view engine', 'html');
+app.engine(".html", ejs.__express);
+app.set("view engine", "html");
 
 app.use(nocache());
 
-function isRefreshableIdentity(identity){
-  if (!identity || typeof identity !== 'object') {
+function isRefreshableIdentity(identity) {
+  if (!identity || typeof identity !== "object") {
     return false;
   }
   if (!identity.refresh_expires || Date.now() >= identity.refresh_expires) {
@@ -61,18 +63,28 @@ function isRefreshableIdentity(identity){
 async function refreshIdentity(identity) {
   try {
     const response = await axios.get(
-      uid2BaseUrl + '/v1/token/refresh?refresh_token=' + encodeURIComponent(identity.refresh_token),
-      { headers: { 'Authorization': 'Bearer ' + uid2ApiKey } });
-    if (response.data.status === 'optout') {
+      uid2BaseUrl +
+        "/v1/token/refresh?refresh_token=" +
+        encodeURIComponent(identity.refresh_token),
+      { headers: { Authorization: "Bearer " + uid2ApiKey } }
+    );
+    if (response.data.status === "optout") {
       return undefined;
-    } else if (response.data.status !== 'success') {
-      throw new Error('Got unexpected token refresh status: ' + response.data.status);
-    } else if (!isRefreshableIdentity(response.data.body) || response.data.body.identity_expires <= Date.now()) {
-      throw new Error('Invalid identity in token refresh response: ' + response.data);
+    } else if (response.data.status !== "success") {
+      throw new Error(
+        "Got unexpected token refresh status: " + response.data.status
+      );
+    } else if (
+      !isRefreshableIdentity(response.data.body) ||
+      response.data.body.identity_expires <= Date.now()
+    ) {
+      throw new Error(
+        "Invalid identity in token refresh response: " + response.data
+      );
     }
     return response.data.body;
   } catch (err) {
-    console.error('Identity refresh failed: ' + err);
+    console.error("Identity refresh failed: " + err);
     return Date.now() >= identity.identity_expires ? undefined : identity;
   }
 }
@@ -80,68 +92,82 @@ async function verifyIdentity(req) {
   if (!isRefreshableIdentity(req.session.identity)) {
     return false;
   }
-  if (Date.now() >= req.session.identity.refresh_from || Date.now() >= req.session.identity.identity_expires) {
+  if (
+    Date.now() >= req.session.identity.refresh_from ||
+    Date.now() >= req.session.identity.identity_expires
+  ) {
     req.session.identity = await refreshIdentity(req.session.identity);
     return !!req.session.identity;
   }
   return !!req.session.identity;
 }
-async function protected(req, res, next){
+async function protected(req, res, next) {
   if (await verifyIdentity(req)) {
     next();
   } else {
     req.session = null;
-    res.redirect('/login');
+    res.redirect("/login");
   }
 }
 
-app.get('/', protected, (req, res) => {
-  res.render('index', { identity: req.session.identity });
-});
-app.get('/content1', protected, (req, res) => {
-  res.render('content', { identity: req.session.identity, content: 'First Sample Content' });
-});
-app.get('/content2', protected, (req, res) => {
-  res.render('content', { identity: req.session.identity, content: 'Second Sample Content' });
-});
-app.get('/getFreshToken', protected, async (req, res) => {
-  if (Date.now() >= req.session.identity.refresh_from || Date.now() >= req.session.identity.identity_expires) {
-    req.session.identity = await refreshIdentity(req.session.identity);
-    res.cookie('identity', JSON.stringify(req.session.identity));
-  }
-  res.json(req.session.identity)
+app.get("/", protected, (req, res) => {
+  res.render("index", { identity: req.session.identity });
 });
 
-app.get('/login', async (req, res) => {
+app.get("/getFreshToken", protected, async (req, res) => {
+  if (
+    Date.now() >= req.session.identity.refresh_from ||
+    Date.now() >= req.session.identity.identity_expires
+  ) {
+    req.session.identity = await refreshIdentity(req.session.identity);
+    res.cookie("identity", JSON.stringify(req.session.identity));
+  }
+  res.json(req.session.identity);
+});
+
+app.get("/login", async (req, res) => {
   if (await verifyIdentity(req)) {
-    res.redirect('/');
+    res.redirect("/");
   } else {
-    res.cookie('identity', '');
+    res.cookie("identity", "");
     req.session = null;
-    res.render('login');
+    res.render("login");
   }
 });
-app.post('/login', (req, res) => {
-  axios.get(uid2BaseUrl + '/v1/token/generate?email=' + encodeURIComponent(req.body.email), { headers: { 'Authorization': 'Bearer ' + uid2ApiKey } })
+app.post("/login", (req, res) => {
+  axios
+    .get(
+      uid2BaseUrl +
+        "/v1/token/generate?email=" +
+        encodeURIComponent(req.body.email),
+      { headers: { Authorization: "Bearer " + uid2ApiKey } }
+    )
     .then((response) => {
-      if (response.data.status !== 'success') {
-        res.render('error', { error: 'Got unexpected token generate status: ' + response.data.status, response: response });
-      } else if (typeof response.data.body !== 'object') {
-        res.render('error', { error: 'Unexpected token generate response format: ' + response.data, response: response });
+      if (response.data.status !== "success") {
+        res.render("error", {
+          error:
+            "Got unexpected token generate status: " + response.data.status,
+          response: response,
+        });
+      } else if (typeof response.data.body !== "object") {
+        res.render("error", {
+          error: "Unexpected token generate response format: " + response.data,
+          response: response,
+        });
       } else {
         req.session.identity = response.data.body;
-        res.cookie('identity', JSON.stringify(response.data.body));
-        res.redirect('/');
+        res.cookie("identity", JSON.stringify(response.data.body));
+        res.redirect("/");
       }
     })
     .catch((error) => {
-        res.render('error', { error: error, response: error.response });
+      res.render("error", { error: error, response: error.response });
     });
 });
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   req.session = null;
-  res.cookie('identity', '');
-  res.redirect('/login');
+  res.cookie("identity", "");
+  res.redirect("/login");
 });
 
 app.listen(port, () => {
