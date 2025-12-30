bac71009051129fee77593c502491c58eaf8114a
Eiman Eltigani <v-eiman.eltigani@thetradedesk.com>
2025-12-30 11:00:56 -0700

    remove deprecated sample sites from examples folder

--- examples/google-secure-signals-integration/react_client_side/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import SecureSignalsApp from './SecureSignalsApp.tsx';
-
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(<SecureSignalsApp />);
